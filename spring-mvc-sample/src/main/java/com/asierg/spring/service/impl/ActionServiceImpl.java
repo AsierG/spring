b996f2fbca6c4c4eaab8b94bce32b63cf73ad719
AsierG <asiergrc@gmail.com>
2018-04-08 17:56:32 +0200

    test changes

--- spring-mvc-sample/src/main/java/com/asierg/spring/service/impl/ActionServiceImpl.java
+++ spring-mvc-sample/src/main/java/com/asierg/spring/service/impl/ActionServiceImpl.java
@@ -1,32 +1,35 @@
 package com.asierg.spring.service.impl;
 
-import java.util.List;
-
+import com.asierg.spring.db.repository.ActionJpaRepository;
+import com.asierg.spring.model.Action;
+import com.asierg.spring.service.ActionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.asierg.spring.db.repository.ActionJpaRepository;
-import com.asierg.spring.model.Action;
-import com.asierg.spring.service.ActionService;
+import java.util.List;
 
 @Service
 public class ActionServiceImpl implements ActionService {
 
-	@Autowired
-	private ActionJpaRepository actionJpaRepository;
+    private ActionJpaRepository actionJpaRepository;
 
-	@Override
-	@Transactional(propagation = Propagation.REQUIRED)
-	public Action createAction(Action action) {
-		return actionJpaRepository.save(action);
-	}
+    @Autowired
+    public ActionServiceImpl(ActionJpaRepository actionJpaRepository) {
+        this.actionJpaRepository = actionJpaRepository;
+    }
 
-	@Override
-	@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
-	public List<Action> findAllOrderByDateAsc() {
-		return actionJpaRepository.findAll();
-	}
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Action createAction(Action action) {
+        return actionJpaRepository.save(action);
+    }
+
+    @Override
+    @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
+    public List<Action> findAllOrderByDateAsc() {
+        return actionJpaRepository.findAll();
+    }
 
 }
